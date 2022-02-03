203bb9951ef8ba3bd4333491304e955f338e43a7
hisanori-long <shimakawa.hisanori@gmail.com>
2022-02-04 00:13:20 +0900

    create projects model

---
+++ database/migrations/2022_02_03_151009_create_projects_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->integer("client_id");
+            $table->string("ar_url");
+            $table->string("public_url");
+            $table->string("name");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
