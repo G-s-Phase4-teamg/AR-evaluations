517fda4ed987d47a3cf42a4bdbb2ff37c226b519
hisanori-long <shimakawa.hisanori@gmail.com>
2022-02-09 13:00:19 +0900

    edit client.projects (temp)

--- app/Http/Controllers/Clientcontroller.php
+++ app/Http/Controllers/Clientcontroller.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Users;
+use Auth;
 
 class Clientcontroller extends Controller
 {
     function projects(){
+        $projects =Users::find(Auth::user()->id)->get_projects;
+        dd($projects);
         return view("client.projects");
     }
 }
--- app/Models/users.php
+++ app/Models/users.php
@@ -4,8 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Users;
 
 class users extends Model
 {
     use HasFactory;
+    public function get_projects(){
+        return $this->hasMany(Users::class);
+    }
 }
