a1c7a2d8a6d51d0a65f803affa197ec795c6eb3e
hisanori-long <shimakawa.hisanori@gmail.com>
2022-02-09 23:40:38 +0900

    edit instagram controller

--- app/Http/Controllers/Clientcontroller.php
+++ app/Http/Controllers/Clientcontroller.php
@@ -18,8 +18,8 @@ class Clientcontroller extends Controller
 
     function survey(Request $request){
         //DBよりデータの取得
-        $questions_m =new Questions(); //questionsをインスタンス化
-        $questions =$questions_m->get_questions($request->project_id); //get_questions関数を実行(models/questions)
+        $questions_m =new Questions(); //questionsをインスタンス化(models/questions)
+        $questions =$questions_m->get_questions($request->project_id); //get_questions関数を実行
         $question_id =array_column($questions->toArray(), "id"); //questionsのidのみを配列として格納
         $choices =$questions_m->get_choices($question_id); //question_idを使い、choicesを抽出
         $choice_answers =$questions_m->get_choice_answers($question_id); //question_idを使い、choice_answersを抽出
@@ -36,8 +36,9 @@ class Clientcontroller extends Controller
     }
 
     function instagram(Request $request){
-        $hushtags_m =new Hushtags();
-        $hushtags =$hushtags_m->get_hushtags($request->project_id);
+        //DBよりデータの取得
+        $hushtags_m =new Hushtags(); //hushtagsをインスタンス化(models/hushtags)
+        $hushtags =$hushtags_m->get_hushtags($request->project_id); //get_hushtags関数を実行
 
         return view("client.instagram",[
             "hushtags" =>$hushtags,
