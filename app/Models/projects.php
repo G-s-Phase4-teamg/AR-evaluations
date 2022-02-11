562b8e5ee747aae81035be5abc57981e80793c7f
hisanori-long <shimakawa.hisanori@gmail.com>
2022-02-12 05:48:40 +0900

    add feature instagram_api

--- app/Models/projects.php
+++ app/Models/projects.php
@@ -5,17 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Hushtags;
+use App\Http\Controllers\ApiController;
 
 class projects extends Model
 {
     use HasFactory;
 
     public function store_api(){
+        $api_m=new ApiController; //apiコントローラをインスタンス化
         $now =now(); //現在時刻を取得
         $project_ids=Projects::select("id")->where("released_at", "<", $now)->where("closed_at", ">", $now)->get();//released_at<現在時刻<closed_atのproject_idを取得
         $hushtags=Hushtags::whereIn("project_id", $project_ids)->get(); //project_idsに含まれるハッシュタグを所得
         foreach ($hushtags as $hushtag){
-            echo $hushtag;
+            $contributions= $api_m->index($hushtag -> hushtag);
+            dd($contributions);
         }
         
     }
---
+++ config/instagram_api.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'instagram_api_id' => env('INSTAGRAM_API_ID', null),
+    'instagram_api_url' => env('INSTAGRAM_API_URL', null),
+    'instagram_api_token' => env('INSTAGRAM_API_TOKEN', null),
+];
