e650dc51299d9fdcc0437f3e14141e712d788efb
hisanori-long <shimakawa.hisanori@gmail.com>
2022-02-11 20:30:29 +0900

    add groupby at get_choice_answers

--- app/Models/questions.php
+++ app/Models/questions.php
@@ -22,7 +22,8 @@ class questions extends Model
         return $choices;
     }
     public function get_choice_answers($question_id){ //question_idに含まれるデータをchoice_answersテーブルから抽出
-        $choice_answers=Choice_answers::whereIn("question_id", $question_id)->get();
+        $choice_answers=Choice_answers::select("choice_id")->selectRaw('COUNT(customer_id) as count_answer')->whereIn("question_id", $question_id)->groupby("choice_id")->get();
+        //choice_idでグループ化し、各選択肢の回答数を算出
         return $choice_answers;
     }
     public function get_text_answers($question_id){ //question_idに含まれるデータをtext_answersテーブルから抽出
